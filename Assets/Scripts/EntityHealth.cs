756a0bc284722da160175d5f683cbad886f01237
foxfluff <foxfluff@users.noreply.github.com>
2016-12-23 21:22:26 -0500

    Initial Commit. Basic functionality implemented

---
+++ Assets/Scripts/EntityHealth.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityHealth : MonoBehaviour
+{
+
+	[SerializeField]
+	int MaxHealth;
+
+	[SerializeField]
+	bool IsInvincible;
+
+	int health { get; set; }
+
+	// Use this for initialization
+	void Start()
+	{
+		health = MaxHealth;
+	}
+
+	void DamageEntity(int value)
+	{
+		if (!IsInvincible)
+		{
+			health -= value;
+			if (health <= 0)
+				KillEntity();
+		}
+	}
+
+	void KillEntity()
+	{
+
+	}
+
+}
